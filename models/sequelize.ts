723d163f279976c7ef92d6a0f646fdd208596853
ShahdKhader <shahdmahez@gmail.com>
2023-10-13 21:29:57 +0300

    edit dialect

--- models/sequelize.ts
+++ models/sequelize.ts
@@ -4,7 +4,8 @@ import configlist from '../config/config';
 const nodeenv = process.env.NODE_ENV as 'development' | 'test' | 'production'
 const config = configlist[nodeenv || 'development']
 console.log('DB_CONFIG:', config);
-const dburl = `${config.dialect}://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`;
+const dialect = config.dialect || 'mysql';
+const dburl = `${dialect}://${config.username}:${config.password}@${config.host}:${config.port}/${config.database}`;
 
 const sequelize = new Sequelize(dburl);
 
